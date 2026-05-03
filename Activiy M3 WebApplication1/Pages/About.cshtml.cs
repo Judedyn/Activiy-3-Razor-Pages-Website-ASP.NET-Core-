9d336dd03b8a0ba8a299727db82ce8c9da966c8c
frans <frans@NIKKO>
2026-05-03 00:00:55 -0600

    Add project files.

---
+++ Activiy M3 WebApplication1/Pages/About.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Activiy_M3_WebApplication1.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Activiy M3 WebApplication1/Pages/Contact.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Activiy_M3_WebApplication1.Pages
+{
+    public class ContactModel : PageModel
+    {
+        [BindProperty]
+        public string Name { get; set; } = "";
+
+        [BindProperty]
+        public string Email { get; set; } = "";
+
+        [BindProperty]
+        public string Message { get; set; } = "";
+
+        public string SuccessMessage { get; set; } = "";
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            SuccessMessage = "Thank you for contacting me!";
+        }
+    }
+}
